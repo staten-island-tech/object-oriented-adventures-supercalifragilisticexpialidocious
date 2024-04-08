92d561f2b1eb201602da1f8e6ac9fe0d1e1d3946
sarahshaoss <sarahs389@nycstudents.net>
2024-04-07 20:57:49 -0400

    savetojson

--- inventorycode.py
+++ inventorycode.py
@@ -1,16 +1,13 @@
 import json
 
-with open("./inventory.json", "r") as f:
-    data = json.load(f)
-items = open("./data.json", encoding="utf8")
-inv = json.load(items)
-
-
-class Inventory:
-    def inv(self):
-        data.append()
-
-
-with open("data.json", "w") as f:
-    json_string = json.dumps(data, indent= 4)
-    f.write(json_string)
+class Save:
+    def LoadJson(self):
+        with open("./inventory.json", "r") as f:
+            data = json.load(f)
+    def Open(self):
+        items = open("./data.json", encoding="utf8")
+        inv = json.load(items)
+    def SaveToJson(self):
+        with open("data.json", "w") as f:
+            json_string = json.dumps(data, indent= 4)
+            f.write(json_string)
