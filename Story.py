9972f28983c9f3732f6288c747ed6557110b1416
sarahshaoss <sarahs389@nycstudents.net>
2024-05-09 12:14:10 -0400

    fight

--- Story.py
+++ Story.py
@@ -1,4 +1,4 @@
-import json, time
+import time
 """with open("./inventory.json", "r") as f:
     inventory = json.load(f) #weapons, pets, badges, armor
 with open("./profile.json", "r") as f:
@@ -7,24 +7,50 @@ usernames = open("./users.json", encoding="utf8")
 user = json.load(usernames) """
 
 
-""" #Battle dialogue:
-print(f"You have encountered a !")
+""" def BattleDialogueTundra(): """
+
+Enemy = "Arctic Fox"
+EnemyHealth = 100
+playerhealth = 100
+umbrella = 20
+
+print(f"You have encountered a {Enemy}!")
 choice = input("[1] > Fight\n[2] > Run away\n> ")
- """
+if choice == 1:
+    print(f"{Enemy} dealt 10 damage")
+    playerhealth - 10
+    fight = input("[1] > Use your weapon\n[2] > Punch\n[3] > Kick")
+    if fight == 1:
+        print(f"You dealt {umbrella} damage")
+        EnemyHealth - umbrella
+    elif fight == 2:
+        print("You dealt 5 damage")
+        EnemyHealth - 5
+    elif fight == 3:
+        print("You dealt 5 damage")
+        EnemyHealth - 5
+    print(playerhealth)
+    print(EnemyHealth)
+
+elif choice == 2:
+    print("Scaredy cat")
+
 
 
 #ADHD npc dialogue:
-print("Knock Knock")
-o = input("[1] > Who's there?\n[2] > Go away hippie\n> ")
-if o == "1":
-    input("Hatch\n> ")
-    print("Bless you! AH HA")
-    time.sleep(.7)
-    print("AH HA.")
-    time.sleep(.7)
-    print("AH HA.")
-elif o == "2":
-    print("ok kys")
+
+""" def ADHDNPC():
+    print("Knock Knock")
+    o = input("[1] > Who's there?\n[2] > Go away hippie\n> ")
+    if o == "1":
+        input("Hatch\n> ")
+        print("Bless you! AH HA")
+        time.sleep(.7)
+        print("AH HA.")
+        time.sleep(.7)
+        print("AH HA.")
+    elif o == "2":
+        print("ok kys") """
 
 
 """ 
