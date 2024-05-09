bd9f52f77a7d460a728192e73bae1559dbbc1b8d
sarahshaoss <sarahs389@nycstudents.net>
2024-05-09 12:20:16 -0400

    enemyfight test

--- Story.py
+++ Story.py
@@ -1,4 +1,4 @@
-import time
+import time, random
 """with open("./inventory.json", "r") as f:
     inventory = json.load(f) #weapons, pets, badges, armor
 with open("./profile.json", "r") as f:
@@ -8,24 +8,27 @@ user = json.load(usernames) """
 
 
 """ def BattleDialogueTundra(): """
+random.randint(10,15)
+
 
 Enemy = "Arctic Fox"
 EnemyHealth = 100
 playerhealth = 100
 umbrella = 20
 
+
 print(f"You have encountered a {Enemy}!")
-choice = input("[1] > Fight\n[2] > Run away\n> ")
+choice = int(input("[1] > Fight\n[2] > Run away\n> "))
 if choice == 1:
     print(f"{Enemy} dealt 10 damage")
-    playerhealth - 10
-    fight = input("[1] > Use your weapon\n[2] > Punch\n[3] > Kick")
+    playerhealth = playerhealth - 10
+    fight = input("[1] > Use your weapon\n[2] > Punch\n[3] > Kick\n> ")
     if fight == 1:
         print(f"You dealt {umbrella} damage")
-        EnemyHealth - umbrella
+        EnemyHealth = EnemyHealth - umbrella
     elif fight == 2:
         print("You dealt 5 damage")
-        EnemyHealth - 5
+        EnemyHealth = EnemyHealth - 5
     elif fight == 3:
         print("You dealt 5 damage")
         EnemyHealth - 5
