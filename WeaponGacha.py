7f67bdc74b456ad9546129abd0120a44e292b5d8
Wen-Qian-Zheng <158204243+Wen-Qian-Zheng@users.noreply.github.com>
2024-06-06 21:39:45 -0400

    a

--- WeaponGacha.py
+++ WeaponGacha.py
@@ -2,21 +2,19 @@ import random
 import json
 
 class WeaponGacha:
-    def __init__(self):
+    def init(self):
         with open("Weapons.json", encoding="utf8") as file:
             self.weapons_data = json.load(file)
 
-        self.drop_rates = [
-            {"???": 0.05},
-            {"Mythical": 1.95},
-            {"Legendary": 5.5},
-            {"Ultra": 10},
-            {"Epic": 17.5},
-            {"Rare": 27.5},
-            {"Common": 37.5}
-        ]
-
-        self.weights = [0.05, 1.95, 5.5, 10, 17.5, 27.5, 37.5]
+        self.drop_rates = {
+            "???": 0.05,
+            "Mythical": 1.95,
+            "Legendary": 5.5,
+            "Ultra": 10,
+            "Epic": 17.5,
+            "Rare": 27.5,
+            "Common": 37.5
+        }
 
     def add_item_to_inventory(self, item):
         with open("Inventory.json", "r") as file:
@@ -35,33 +33,9 @@ class WeaponGacha:
             json.dump(inventory, file, indent=4)
 
     def gacha(self):
-        print("Welcome to the Weapon Gacha!")
-        choice = input("Would you like to use your gacha ticket? (yes/no): ")
-        if choice.lower() == "yes":
-            random_number = random.uniform(0, 100)
-            cumulative_probability = 0
-            selected_rarity = None
-            for rarity, probability in self.drop_rates.items():
-                cumulative_probability += probability
-                if random_number <= cumulative_probability:
-                    selected_rarity = rarity
-                    break
-            
-            if selected_rarity:
-                weapons_in_rarity = self.drop_rates[selected_rarity]
-                if weapons_in_rarity:
-                    selected_weapon = random.choices([self.drop_rates], weights = self.weights)
-                    print(selected_weapon)
-                    print(f"You just obtained a {selected_weapon['WeaponName']}!")
-                    self.add_item_to_inventory(selected_weapon)
-                else:
-                    print("No weapons available in this rarity.")
-            else:
-                print("No weapon obtained.")
-        elif choice.lower() == "no":
-            print("Exiting gacha.")
-        else:
-            print("Invalid choice. Exiting gacha.")
+        random_number = random.uniform(0, 100)
+        cumulative_probability = 0
+        selected_rarity = None
 
         for rarity, probability in self.drop_rates.items():
             cumulative_probability += probability
@@ -69,7 +43,7 @@ class WeaponGacha:
             if random_number <= cumulative_probability:
                 selected_rarity = rarity
                 break
-        
+
         if selected_rarity:
             weapons_in_rarity = self.weapons_data[selected_rarity]["Weapons"]
 
@@ -119,11 +93,12 @@ class ArmorGacha:
                 break
         
         if selected_rarity:
-            armors_in_rarity = self.armors_data[selected_rarity]["Armors"]
-            if armors_in_rarity:
-                selected_armor = random.choice(armors_in_rarity)
-                print(f"You just obtained a {selected_armor['ArmorName']} ({selected_rarity})!")
-                self.add_item_to_inventory(selected_armor)
+            weapons_in_rarity = self.weapons_data[selected_rarity]["Armor"]
+
+            if weapons_in_rarity:
+                selected_weapon = random.choice(weapons_in_rarity)
+                print(f"You just obtained a {selected_weapon['ArmorName']} ({selected_rarity})!")
+                self.add_item_to_inventory(selected_weapon)
 
 class PetGacha:
     def __init__(self):
